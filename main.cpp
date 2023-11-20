8eb972f477a198c756c1a085e355c9cc94f1f5aa
Irell Zane <itsirellzane@gmail.com>
2023-11-20 18:30:52 +0000

    move everything to a single file

--- main.cpp
+++ main.cpp
@@ -2,11 +2,92 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include "Plate.h"
-#include "Piece.h"
-#include "PlateValueCalculator.h"
 
 using namespace std;
+
+struct Piece {
+    int length;
+    int width;
+    int value;
+};
+
+struct Plate {
+    int width;
+    int length;
+};
+
+class PlateValueCalculator {
+    Plate plate;
+    vector<Piece> pieces;
+    vector<vector<int>> maxSubPlateValues;
+    int subPlateLength;
+    int subPlateWidth;
+
+    public:
+        PlateValueCalculator(Plate plate, vector<Piece> pieces) {
+            this->plate = plate;
+            this->pieces = pieces;
+        }
+
+        int calculateVerticalBisectionValue(int slicePosition) {
+            return maxSubPlateValues[subPlateLength][slicePosition] + maxSubPlateValues[subPlateLength][subPlateWidth - slicePosition];
+        }
+
+        int calculateHorizontalBisectionValue(int slicePosition) {
+            return maxSubPlateValues[slicePosition][subPlateWidth] + maxSubPlateValues[subPlateLength - slicePosition][subPlateWidth];
+        }
+
+        int calculateMaxBisectionValue() {
+            int maxValue = 0;
+            int currValue;
+            for (int verticalSlice = 1; verticalSlice < subPlateWidth; verticalSlice++) {
+                currValue = calculateVerticalBisectionValue(verticalSlice);
+                if (currValue > maxValue) {
+                    maxValue = currValue;
+                }
+            }
+
+            for (int horizontalSlice = 1; horizontalSlice < subPlateLength; horizontalSlice++) {
+                currValue = calculateHorizontalBisectionValue(horizontalSlice);
+                if (currValue > maxValue) {
+                    maxValue = currValue;
+                }
+                /* cout << horizontalSlice << "," << subPlateWidth << ":" << maxSubPlateValues[horizontalSlice][subPlateWidth] << "+" << maxSubPlateValues[subPlateLength - horizontalSlice][subPlateWidth] << " "; */
+            }
+
+            return maxValue;
+        }
+
+        int getPieceValueWithSubPlateDimensions() {
+            for (Piece piece : pieces) {
+                if (
+                    (piece.width == subPlateWidth && piece.length == subPlateLength) || 
+                    (piece.width == subPlateLength && piece.length == subPlateWidth) // peças podem girar
+                    ) {
+                    return piece.value;
+                }
+            }
+            return 0;
+        }
+
+        int calculateMaxSubPlateValue() {
+            return max(getPieceValueWithSubPlateDimensions(), calculateMaxBisectionValue());
+        }
+
+        int calculateValue() {
+            maxSubPlateValues = vector<vector<int>>(plate.length + 1, vector<int>(plate.width + 1, 0));
+            for (subPlateLength = 1; subPlateLength <= plate.length; subPlateLength++) {
+                for (subPlateWidth = 1; subPlateWidth <= plate.width; subPlateWidth++) {
+                    maxSubPlateValues[subPlateLength][subPlateWidth] = calculateMaxSubPlateValue();
+                }
+            }
+            return maxSubPlateValues[plate.length][plate.width];
+        }
+
+
+};
+
+
 
 Plate readPlate() {
     Plate plate;
@@ -38,5 +119,4 @@ int main() {
     vector<Piece> pieces = readPieces(numPieces);
     PlateValueCalculator plateValueCalculator(plate, pieces);
     cout << plateValueCalculator.calculateValue() << endl;
-    return 0;
 }
